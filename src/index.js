b715a152074e7082e88bfd04f010ab0a5ea6d4cc
Frank Pagan <frank@cocreate.app>
2021-10-14 17:16:32 +0000

    fix: moved common functions to index.js to make more accessible to other components

--- src/index.js
+++ src/index.js
@@ -1,66 +1,64 @@
+(function(root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(["@cocreate/uuid"], function(uuid) {
+			return factory(window, WebSocket, Blob, uuid)
+		});
+	}
+	else if (typeof module === 'object' && module.exports) {
+		let wndObj = {
+			location: {
+				protocol: ""
+			}
+		};
+		const ws = require("ws");
+		const uuid = require("@cocreate/uuid");
+		module.exports = factory(wndObj, ws, null, uuid);
+	}
+	else {
+		// Browser globals (root is window)
+		root.returnExports = factory(window, WebSocket, Blob, root["@cocreate/socket-client"]);
+	}
+}(typeof self !== 'undefined' ? self : this, function(wnd, WebSocket, Blob, uuid) {
 
-
-(function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(['./common-fun.js', "@cocreate/uuid"], function(commonFunc, uuid) {
-        	return factory(commonFunc, window, WebSocket, Blob, uuid)
-        });
-    } else if (typeof module === 'object' && module.exports) {
-        let wndObj = {
-        	location: {
-        		protocol: ""
-        	}
-        }
-        const ws = require("ws")
-        const commonFunc = require("./common-fun.js")
-        const uuid = require("@cocreate/uuid");
-    	module.exports = factory(commonFunc, wndObj, ws, null, uuid);
-    } else {
-        // Browser globals (root is window)
-        root.returnExports = factory(root["./common-fun.js"], window, WebSocket, Blob, root["@cocreate/socket-client"]);
-  }
-}(typeof self !== 'undefined' ? self : this, function (commonFunc, wnd, WebSocket, Blob, uuid) {
-
-    class CoCreateSocketClient
-	{
+	class CoCreateSocketClient {
 		constructor(prefix = "crud") {
 			this.prefix = prefix || "crud";
 			this.sockets = new Map();
 			this.listeners = new Map();
-			this.messageQueue =  new Map();
-			this.saveFileName =  '';
-			this.globalScope =  "";
+			this.messageQueue = new Map();
+			this.saveFileName = '';
+			this.globalScope = "";
 		}
-	
+
 		setGlobalScope(scope) {
 			this.globalScope = `${this.prefix}/${scope}`;
 		}
-		
+
 		getGlobalScope() {
 			return this.globalScope;
 		}
-		
+
 		/**
 		 * config: {namespace, room, host}
 		 */
-		create (config) {
-			
-			const {namespace, room} = config;
+		create(config) {
+
+			const { namespace, room } = config;
 			const key = this.getKey(namespace, room);
 			let _this = this;
 			if (namespace) {
-				this.setGlobalScope(namespace)
+				this.setGlobalScope(namespace);
 			}
-			
+
 			let socket;
 			if (this.sockets.get(key)) {
 				socket = this.sockets.get(key);
 				// console.log('SOcket already has been register');
 				return;
 			}
-			
-			let w_protocol = wnd.location.protocol;		
+
+			let w_protocol = wnd.location.protocol;
 			if (wnd.location.protocol === "about:") {
 				w_protocol = wnd.parent.location.protocol;
 				if (!config.host) {
@@ -68,19 +66,21 @@
 				}
 			}
 			let protocol = w_protocol === 'http:' ? 'ws' : 'wss';
-			
+
 			const portPrefix = config.port ? `:${config.port}` : '';
-			
+
 			let socket_url = `${protocol}://${wnd.location.host}${portPrefix}/${key}`;
-			
+
 			if (config.host) {
 				if (config.host.includes("://")) {
 					socket_url = `${config.host}/${key}`;
-				} else {
+				}
+				else {
 					if (config.host.includes(":")) {
 						socket_url = `${protocol}://${config.host}/${key}`;
-					} else {
-						socket_url = `${protocol}://${config.host}${portPrefix}/${key}`;	
+					}
+					else {
+						socket_url = `${protocol}://${config.host}${portPrefix}/${key}`;
 					}
 				}
 			}
@@ -92,38 +92,39 @@
 				socket = new WebSocket(socket_url, token);
 				socket.cocreate_connected = false;
 				this.sockets.set(key, socket);
-			} catch(error) {
-				console.log(error)
+			}
+			catch (error) {
+				console.log(error);
 				return;
 			}
 
 			socket.onopen = function(event) {
 				if (!socket.cocreate_connected) {
-					socket.cocreate_connected = true
+					socket.cocreate_connected = true;
 				}
 				const messages = _this.messageQueue.get(key) || [];
 				messages.forEach(msg => socket.send(JSON.stringify(msg)));
 				_this.messageQueue.set(key, []);
-			}
-			
+			};
+
 			socket.onclose = function(event) {
-				switch(event.code) {
+				switch (event.code) {
 					case 1000: // close normal
 						console.log("websocket: closed");
 						break;
-					default: 
+					default:
 						_this.destroy(socket, key);
 						_this.reconnect(socket, config);
 						break;
 				}
-			}
-			
+			};
+
 			socket.onerror = function(err) {
-				console.log(err.message)
+				console.log(err.message);
 				_this.destroy(socket, key);
 				_this.reconnect(socket, config);
-			}
-	
+			};
+
 			socket.onmessage = function(data) {
 				try {
 					if (wnd.Blob) {
@@ -133,11 +134,11 @@
 						}
 					}
 					let rev_data = JSON.parse(data.data);
-					
+
 					//. uid's event
 
 					if (rev_data.data) {
-						
+
 						if (rev_data.data.uid) {
 							_this.__fireEvent(rev_data.data.uid, rev_data.data);
 						}
@@ -145,7 +146,7 @@
 							_this.__fireEvent(rev_data.data.event, rev_data.data);
 							return;
 						}
-						
+
 					}
 					let action = rev_data.action;
 					const listeners = _this.listeners.get(rev_data.action);
@@ -154,136 +155,180 @@
 					}
 					listeners.forEach(listener => {
 						listener(rev_data.data, key);
-					})
-				} catch (e) {
+					});
+				}
+				catch (e) {
 					console.log(e);
 				}
-			}
+			};
 		}
-		
+
 		__fireEvent(event_id, data) {
 			if (wnd.CustomEvent) {
 				var event = new wnd.CustomEvent(event_id, {
 					detail: data
-				})
+				});
 				wnd.document.dispatchEvent(event);
-			} else {
-				process.emit(event_id, data)
+			}
+			else {
+				process.emit(event_id, data);
 			}
 		}
-		
+
 		/**
 		 * 
 		 */
-		send (action, data, room) {
+		send(action, data, room) {
 			const request_id = uuid.generate();
 			const obj = {
 				action: action,
-				data: {...data, uid: request_id}
-			}
+				data: { ...data, uid: request_id }
+			};
 			const key = this.getKeyByRoom(room);
 			const socket = this.getByRoom(room);
 
 			if (socket && socket.cocreate_connected) {
 				socket.send(JSON.stringify(obj));
-			} else {
+			}
+			else {
 				if (this.messageQueue.get(key)) {
 					this.messageQueue.get(key).push(obj);
-				} else {
+				}
+				else {
 					this.messageQueue.set(key, [obj]);
 				}
 			}
 			return request_id;
 		}
-		
-		sendFile (file, room) {
+
+		sendFile(file, room) {
 			const socket = this.getByRoom(room);
 			if (socket) {
 				socket.send(file);
 			}
 		}
-	
+
 		/**
 		 * scope: ns/room
 		 */
 		listen(type, callback) {
 			if (!this.listeners.get(type)) {
 				this.listeners.set(type, [callback]);
-			} else {
+			}
+			else {
 				this.listeners.get(type).push(callback);
 			}
 		}
-		
+
 		// ToDo: Apply a backoff 
 		reconnect(socket, config) {
 			let _this = this;
 			setTimeout(function() {
 				_this.create(config);
-			}, 1000)
+			}, 1000);
 		}
-		
+
 		destroy(socket, key) {
 			if (socket) {
 				socket.onerror = socket.onopen = socket.onclose = null;
 				socket.close();
 				socket = null;
 			}
-			
+
 			if (this.sockets.get(key)) {
 				this.sockets.delete(key);
 			}
 		}
-		
+
 		destroyByKey(key) {
-			let socket = this.sockets.get(key) 
+			let socket = this.sockets.get(key);
 			if (socket) {
 				this.destroy(socket, key);
 			}
 		}
-		
+
 		getKey(namespace, room) {
 			let key = `${this.prefix}`;
 			if (namespace && namespace != '') {
-				if (room &&  room != '') {
+				if (room && room != '') {
 					key += `/${namespace}/${room}`;
-				} else {
-					key +=`/${namespace}`;
+				}
+				else {
+					key += `/${namespace}`;
 				}
 			}
 			return key;
 		}
-		
+
 		getByRoom(room) {
-			let key = this.getKeyByRoom(room)
-			return this.sockets.get(key);	
+			let key = this.getKeyByRoom(room);
+			return this.sockets.get(key);
 		}
-		
+
 		getKeyByRoom(room) {
 			let key = this.globalScope;
 			if (room) {
 				key = `${this.prefix}/${room}`;
 			}
-			return key;		
+			return key;
 		}
-		
+
 		listenAsync(eventname) {
 			return new Promise((resolve, reject) => {
-				let wait  = setTimeout(() => {
+				let wait = setTimeout(() => {
 					clearTimeout(wait);
 					resolve(null);
-				}, 5000)
-				
+				}, 5000);
+
 				if (wnd.document) { //. browser case
 					wnd.document.addEventListener(eventname, function(event) {
-					    resolve(event.detail);
-					}, { once: true })
-				} else { //. node case
-					process.once(eventname, (data) => {
-						resolve(data)
-					})
+						resolve(event.detail);
+					}, { once: true });
 				}
-			})
+				else { //. node case
+					process.once(eventname, (data) => {
+						resolve(data);
+					});
+				}
+			});
 		}
+
+		getCommonParams() {
+			let config = {};
+			if (wnd && wnd.config) {
+				config = wnd.config;
+			}
+
+			return {
+				"apiKey": config.apiKey,
+				"organization_id": config.organization_Id,
+			};
+		}
+
+		getCommonParamsExtend(info) {
+			let config = {};
+			if (wnd && wnd.config) config = wnd.config;
+
+			return {
+				"apiKey": info.apiKey || config.apiKey,
+				"organization_id": info.organization_id || config.organization_Id,
+			};
+		}
+
+		generateSocketClient(namespace, room) {
+			let config = {};
+			if (wnd && wnd.config) config = wnd.config;
+
+			let ns = namespace || config.organization_Id;
+			let rr = room || '';
+			if (rr) {
+				return `${ns}/${rr}`;
+			}
+			else {
+				return ns;
+			}
+		}
+
 	}
-    return CoCreateSocketClient
+	return CoCreateSocketClient;
 }));
