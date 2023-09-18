824d8367cc8cfb00caa802f929ef19b45887da09
frankpagan <frank@cocreate.app>
2023-09-17 19:11:36 -0600

    fix:  Add path and pathname

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -8,17 +8,16 @@ module.exports = {
             "object": {
                 "_id": "6143fc8e829b690010a5f338",
                 "name": "index.html",
-                "path": "/docs/socket-client/index.html",
+                "path": "/docs/socket-client",
+                "pathname": "/docs/socket-client/index.html",
                 "src": "{{./docs/index.html}}",
                 "host": [
                     "*",
                     "general.cocreate.app"
                 ],
-                "directory": "/docs/socket-client",
-                "parentDirectory": "{{parentDirectory}}",
+                "directory": "socket-client",
                 "content-type": "{{content-type}}",
-                "public": "true",
-                "website_id": "644d4bff8036fb9d1d1fd69c"
+                "public": "true"
             }
         }
     ]
